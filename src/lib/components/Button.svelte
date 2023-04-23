b2d0c13f1306f74b3bf37d1189046ef04d0ea5ea
John Dave Decano <johndavedecano@gmail.com>
2023-04-23 18:53:41 +0200

    button start

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+	import cx from 'classnames';
+
+	export let size = ''; // lg, md, sm
+	export let variant = ''; // filled, tonal, outlined, flat
+	export let color = 'primary'; // primary, seconary, destructive
+	export let disabled = false;
+	export let icon = false;
+
+	$: classes = cx({
+		btn: true
+	});
+</script>
+
+<button class={classes}>
+	<slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '$lib/components/styles.css';
+
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<div>
+	<h1>Move Svelte Components</h1>
+	<p>Component library for the move project</p>
+	<h3>Button</h3>
+	<Button>Primary</Button>
+</div>
