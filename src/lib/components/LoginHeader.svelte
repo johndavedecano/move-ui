d26eb069b25905889c2e749e827ecd3cce3dc965
John Dave Decano <johndavedecano@gmail.com>
2023-04-30 21:45:13 +0200

    wrapper

---
+++ src/lib/components/LoginHeader.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<div class="header">
+	<slot name="prepend" />
+	<a href="/" class="logo">
+		<Icon name="mm-logo" />
+	</a>
+	<slot name="append" />
+</div>
+
+<style>
+	.header {
+		align-items: center;
+		background-color: var(--white);
+		display: flex;
+		height: 96px;
+		left: 0;
+		padding-left: var(--space-6);
+		padding-right: var(--space-6);
+		position: fixed;
+		top: 0;
+		width: 100%;
+	}
+	.logo {
+		align-items: center;
+		display: flex;
+		flex-grow: 1;
+		justify-content: center;
+	}
+</style>
---
+++ src/lib/components/LoginWrapper.svelte
@@ -0,0 +1,53 @@
+<div class="wrapper">
+	{#if $$slots.header}
+		<slot name="header" />
+	{/if}
+	<div class="content">
+		<div class="content-inner">
+			{#if $$slots.content}
+				<slot name="content">
+					<div class="title-5 navy-dark fw-600 mb-2">Enter your email</div>
+					<div class="gray-6 body fw-400 mb-8">
+						Quis ipsum eget ut commodo tristique tristique malesuada augue sed.
+					</div>
+				</slot>
+			{/if}
+		</div>
+	</div>
+	{#if $$slots.actions}
+		<div class="actions">
+			<slot name="actions" />
+		</div>
+	{/if}
+</div>
+
+<style>
+	.wrapper {
+		--bottom-height: 74px;
+		--top-height: 96px;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		justify-content: stretch;
+		width: 100vw;
+	}
+	.content-inner {
+		padding-bottom: var(--bottom-height);
+		padding-left: var(--space-6);
+		padding-right: var(--space-6);
+		padding-top: var(--top-height);
+	}
+	.actions {
+		align-items: center;
+		background-color: var(--white);
+		bottom: 0;
+		display: flex;
+		height: var(--bottom-height);
+		justify-content: center;
+		left: 0;
+		padding-left: var(--space-6);
+		padding-right: var(--space-6);
+		position: fixed;
+		width: 100%;
+	}
+</style>
