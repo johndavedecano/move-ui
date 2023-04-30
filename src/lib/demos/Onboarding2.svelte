0d841e59e4e0394a0d81efeab260c3dbe77b6a67
John Dave Decano <johndavedecano@gmail.com>
2023-04-30 21:55:47 +0200

    onboarding 2

---
+++ src/lib/demos/Onboarding2.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+	import LoginHeader from '$lib/components/LoginHeader.svelte';
+	import LoginWrapper from '$lib/components/LoginWrapper.svelte';
+	import TextField from '$lib/components/TextField.svelte';
+	import TextFieldMessage from '$lib/components/TextFieldMessage.svelte';
+	import Checkbox from '$lib/components/checkbox.svelte';
+</script>
+
+<LoginWrapper>
+	<svelte:fragment slot="header">
+		<LoginHeader>
+			<a href="/pages/onboarding1" slot="prepend">
+				<Icon name="arrow-left" />
+			</a>
+			<div class="pr-6" slot="append" />
+		</LoginHeader>
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<div class="title-5 navy-dark fw-600 mb-2">Enter your password</div>
+		<div class="gray-6 body fw-400 mb-8">
+			Quis ipsum eget ut commodo tristique tristique malesuada augue sed.
+		</div>
+		<form action="">
+			<TextField label="Password" name="password" type="password" value="12345678">
+				<span slot="append">
+					<Icon name="eye" />
+				</span>
+				<span slot="message">
+					<TextFieldMessage customClass="pt-2">At least 8 characters</TextFieldMessage>
+				</span>
+			</TextField>
+			<div class="pt-6">
+				<Checkbox>
+					<span class="caption navy-dark">
+						I have read and agree to the <a href="/" class="coral-dark">terms</a> and
+						<a href="/" class="coral-dark">privacy policy </a>
+					</span>
+				</Checkbox>
+			</div>
+		</form>
+	</svelte:fragment>
+	<svelte:fragment slot="actions">
+		<Button color="tertiary" size="lg" fullWidth>Continue</Button>
+	</svelte:fragment>
+</LoginWrapper>
