77fbe078790a6ce3e07a6c35551a02610168ccd7
John Dave Decano <johndavedecano@gmail.com>
2023-05-04 00:56:36 +0200

    magic link 2

--- src/lib/demos/MagicLink2.svelte
+++ src/lib/demos/MagicLink2.svelte
@@ -3,9 +3,6 @@
 	import Icon from '$lib/components/Icon.svelte';
 	import LoginHeader from '$lib/components/LoginHeader.svelte';
 	import LoginWrapper from '$lib/components/LoginWrapper.svelte';
-	import TextField from '$lib/components/TextField.svelte';
-	import TextFieldMessage from '$lib/components/TextFieldMessage.svelte';
-	import Checkbox from '$lib/components/Checkbox.svelte';
 </script>
 
 <LoginWrapper>
@@ -18,30 +15,20 @@
 		</LoginHeader>
 	</svelte:fragment>
 	<svelte:fragment slot="content">
-		<div class="title-5 navy-dark fw-600 mb-2">Enter your password</div>
-		<div class="gray-6 body fw-400 mb-8">
-			Quis ipsum eget ut commodo tristique tristique malesuada augue sed.
+		<div class="title-5 text-center navy-dark fw-500 mb-4 mt-12">Abracadabra!</div>
+		<div class="text-center flex j-content-center w-400 mb-1">
+			<img src="/magic-link.png" alt="" />
 		</div>
-		<form action="">
-			<TextField label="Password" name="password" type="password" value="12345678">
-				<span slot="append">
-					<Icon name="eye" />
-				</span>
-				<span slot="message">
-					<TextFieldMessage customClass="pt-2">At least 8 characters</TextFieldMessage>
-				</span>
-			</TextField>
-			<div class="pt-6">
-				<Checkbox>
-					<span class="caption navy-dark">
-						I have read and agree to the <a href="/" class="coral-dark">terms</a> and
-						<a href="/" class="coral-dark">privacy policy </a>
-					</span>
-				</Checkbox>
-			</div>
-		</form>
+		<div class="title-4 text-center navy-dark fw-600 mb-4">Check your email</div>
+		<div class="gray-6 text-center body fw-400 mb-8">
+			We sent you an email at <strong class="gray-5">test@test.com</strong>, click the link to be
+			magically logged in - no password needed! Make sure to check spam.
+		</div>
+		<div class="text-right pt-3">
+			<Button color="tertiary" size="md" variant="flat" element="a">Help</Button>
+		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="actions">
-		<Button color="tertiary" size="lg" fullWidth>Continue</Button>
+		<Button color="tertiary" size="lg" fullWidth disabled>Resend (20s)</Button>
 	</svelte:fragment>
 </LoginWrapper>
