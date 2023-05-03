8510c47864258b28f03026c9f87f048747bbc9d1
John Dave Decano <johndavedecano@gmail.com>
2023-05-04 01:01:40 +0200

    magic link pages

--- src/lib/demos/MagicLink4.svelte
+++ src/lib/demos/MagicLink4.svelte
@@ -1,11 +1,11 @@
 <script>
+	// @ts-nocheck
+
 	import Button from '$lib/components/Button.svelte';
 	import Icon from '$lib/components/Icon.svelte';
 	import LoginHeader from '$lib/components/LoginHeader.svelte';
 	import LoginWrapper from '$lib/components/LoginWrapper.svelte';
 	import TextField from '$lib/components/TextField.svelte';
-	import TextFieldMessage from '$lib/components/TextFieldMessage.svelte';
-	import Checkbox from '$lib/components/Checkbox.svelte';
 </script>
 
 <LoginWrapper>
@@ -18,30 +18,33 @@
 		</LoginHeader>
 	</svelte:fragment>
 	<svelte:fragment slot="content">
-		<div class="title-5 navy-dark fw-600 mb-2">Enter your password</div>
-		<div class="gray-6 body fw-400 mb-8">
-			Quis ipsum eget ut commodo tristique tristique malesuada augue sed.
-		</div>
+		<div class="title-5 navy-dark fw-600 mb-2">Log in</div>
+		<div class="gray-6 body fw-400 mb-8">You already have an account with us</div>
 		<form action="">
+			<TextField
+				wrapperClass="mb-4"
+				label="Email"
+				name="email"
+				type="email"
+				value="carla.honrado@test.com"
+				disabled
+			/>
 			<TextField label="Password" name="password" type="password" value="12345678">
 				<span slot="append">
 					<Icon name="eye" />
 				</span>
-				<span slot="message">
-					<TextFieldMessage customClass="pt-2">At least 8 characters</TextFieldMessage>
-				</span>
 			</TextField>
-			<div class="pt-6">
-				<Checkbox>
-					<span class="caption navy-dark">
-						I have read and agree to the <a href="/" class="coral-dark">terms</a> and
-						<a href="/" class="coral-dark">privacy policy </a>
-					</span>
-				</Checkbox>
+
+			<div class="pt-4">
+				<Button color="tertiary" size="lg" fullWidth>Log in</Button>
+			</div>
+
+			<div class="text-center pt-4 pb-8" />
+			<div class="flex a-items-center j-content-center">
+				<a href="/" class="label fw-500 coral-dark">Forgot password</a>
+				<div class="flex-1" />
+				<a href="/" class="label fw-500 coral-dark">Help</a>
 			</div>
 		</form>
-	</svelte:fragment>
-	<svelte:fragment slot="actions">
-		<Button color="tertiary" size="lg" fullWidth>Continue</Button>
 	</svelte:fragment>
 </LoginWrapper>
